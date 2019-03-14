ffe8edae3a2167648062e6286c4358c32755bd28
Sandy Gifford <sanford.r.gifford@gmail.com>
2019-03-14 18:42:06 -0400

    Docs

--- src/utils/DiffUtils.ts
+++ src/utils/DiffUtils.ts
@@ -1,6 +1,12 @@
 import * as Diff from "diff";
 
 export default class DiffUtils {
+	/**
+	 * Determines what fraction different 2 strings are
+	 * @param str1 A string
+	 * @param str2 Another string
+	 * @returns A number between 0 and 1 describing how different the strings are
+	 */
 	public static diffFrac(str1: string, str2: string): number {
 		const diff = Diff.diffChars(str1, str2);
 		const longerLength = Math.max(str1.length, str2.length);
--- src/utils/DomUtils.ts
+++ src/utils/DomUtils.ts
@@ -1,10 +1,21 @@
 export default class DomUtils {
+	/**
+     * Performs the specified action for each ancestor of a given element (inclusive)
+	 * @param element An element
+	 * @param func A callback that's called once for each ancestor (as well as the original element)
+	 */
 	public static forEachAncestor(element: HTMLElement, func: (ancestor: HTMLElement, index: number) => void): void {
 		for (let i = 0; element.parentElement; element = element.parentElement, i++) {
 			func(element, i);
 		}
 	}
 
+	/**
+     * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+	 * @param element An element
+	 * @param mapper A callback that's called once for each ancestor (as well as the original element)
+	 * @returns An array of items
+	 */
 	public static mapAncestors<T>(element: HTMLElement, mapper: (ancestor: HTMLElement, index: number) => T): T[] {
 		const result: T[] = [];
 		this.forEachAncestor(element, (ancestor, i) => result.push(mapper(ancestor, i)));
